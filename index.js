60eb257bcb16a22ea92a0a48096da55df165dcb2
Fnowshin <fnowshin5@gmail.com>
2022-10-26 01:42:19 +0600

    detailed-topic

--- index.js
+++ index.js
@@ -6,13 +6,29 @@ const port = process.env.PORT || 7000;
 app.use(cors());
 
 const topics = require ('./Data/courses.json');
+const details = require('./Data/details.json')
 
-app.get('/', (req, res) => {
-    res.send('marketing Api running');
-});
+// app.get('/', (req, res) => {
+//     res.send('marketing Api running');
+// });
 
 app.get('/course-topics' , (req , res ) => {
     res.send(topics)
+})
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const course_details = details.filter( detail => detail.course_id === id);
+    res.send(course_details);
+})
+
+app.get('/topics', (req, res) => {
+    res.send(topics);
+})
+
+app.get('/details/:id', (req, res) => {
+    const id = req.params.id;
+    const clickedDetails = details.find( detail =>detail._id === id);
+    res.send(clickedDetails);
 })
 
 app.listen(port, () => {
